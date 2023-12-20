56c04f921126128015544e4c2958bd47f8eac2b6
VALENTIN DIAZ <valentin.diaz@uc.cl>
2023-12-20 17:39:20 -0300

    feat: close modal when click outside

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,8 +16,19 @@ function Modal({ openModal, closeModal, children }) {
             onCancel={closeModal}  
             autoFocus 
             onClick={(e) => {
-                console.log('click en el modal')
-                console.log(e)
+                // console.log('click en el modal')
+                // console.log(e)
+                // console.log(ref.current.getBoundingClientRect())
+                const dialogDimentions = ref.current.getBoundingClientRect()
+                if (
+                    e.clientX < dialogDimentions.left ||
+                    e.clientX > dialogDimentions.right ||
+                    e.clientY < dialogDimentions.top ||
+                    e.clientY > dialogDimentions.bottom
+                ) {
+                    // console.log('El click está afuera')
+                    closeModal()
+                }
             }} 
         >
             {children}
