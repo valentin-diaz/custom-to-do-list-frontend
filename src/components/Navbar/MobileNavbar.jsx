70c89d378afb6079f0e83f590b2c86b843e079fb
VALENTIN DIAZ <valentin.diaz@uc.cl>
2023-12-31 12:05:22 -0300

    feat: collapsable hamburger menu with links

--- src/components/Navbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar.jsx
@@ -1,15 +1,16 @@
 import { NavLink } from "react-router-dom";
 import "./MobileNavbar.css"
 import { FaBars } from "react-icons/fa"
+import { useState } from "react";
 
-function MobileNavbar() {
+const HamburgerMenu = ({ visible, setVisible }) => {
     const renderNavItem = (path, label) => {
         return (
             <NavLink
                 to={path}
                 className={({ isActive }) =>
                     [
-                        "navbar-item",
+                        "mobile-navbar-item",
                         isActive ? "active" : "",
                     ].join(" ")
                 }
@@ -19,13 +20,34 @@ function MobileNavbar() {
         )
     }
     
-    return ( 
-        <div className="mobile-navbar-wrapper">
-            <h1 className="mobile-navbar-logo">Logo</h1>
-            <div className="mobile-navbar-button">
-                <FaBars size={30}/>
-            </div>
+    return (
+        <div className={[
+            "hamburger-menu",
+            visible ? "visible" : ""
+        ].join(" ")}>
+            <nav className="mobile-navbar">
+                { renderNavItem('/calendar', 'Calendario') }
+                { renderNavItem('/', 'Tareas Diarias') }
+                { renderNavItem('/time', 'Tiempo') }
+            </nav>
         </div>
+    )
+}
+
+function MobileNavbar() {
+    const [hamburgerMenuVisible, setHamburgerMenuVisible] = useState(false);
+
+    return ( 
+        <>
+            <div className="mobile-navbar-wrapper">
+                <h1 className="mobile-navbar-logo">Logo</h1>
+                <div className="mobile-navbar-button" onClick={() => setHamburgerMenuVisible(prev => !prev)}>
+                    <FaBars size={30}/>
+                </div>
+            </div>
+
+            <HamburgerMenu visible={hamburgerMenuVisible} setVisible={setHamburgerMenuVisible}/>
+        </>
      );
 }
 
