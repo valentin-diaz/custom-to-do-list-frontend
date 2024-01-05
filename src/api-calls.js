ee91ffebd3c7df04491cdc6b43b57d64a5c4b086
VALENTIN DIAZ <valentin.diaz@uc.cl>
2024-01-05 10:07:42 -0300

    refactor: get api url from .env

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = 'http://localhost:5000'
-const BASE_URL = 'https://to-do-list-api-w1pj.onrender.com';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getDailyTasks = async () => {
     console.log('GET DAILY TASKS')
