6f5b8bd3891abdb4bcb273ed06c3e85fbb6e8509
VALENTIN DIAZ <valentin.diaz@uc.cl>
2024-01-19 13:28:30 -0300

    console log url

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
+console.log(`La url es ${BASE_URL}`)
 
 export const getDailyTasks = async () => {
     console.log('GET DAILY TASKS')
